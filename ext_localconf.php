1f3c669c64d8fc9dd574b453817698206b2e0a5a
Steffen Kroggel <kroggel@rkw.de>
2020-07-16 12:14:54 +0200

    [FIX] Fix for logfile path

--- ext_localconf.php
+++ ext_localconf.php
@@ -67,7 +67,7 @@ call_user_func(
                 // add a FileWriter
                 'TYPO3\\CMS\\Core\\Log\\Writer\\FileWriter' => array(
                     // configuration for the writer
-                    'logFile' => 'typo3temp/logs/tx_rkwtools.log'
+                    'logFile' => 'typo3temp/var/logs/tx_rkwtools.log'
                 )
             ),
         );
